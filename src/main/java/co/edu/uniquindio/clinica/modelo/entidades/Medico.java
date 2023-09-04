9035a66a6de3ffc724583c34d711f21f3e6794fa
juanf28 <juan-felipe-00@hotmail.com>
2023-09-04 09:19:19 -0500

    Entidades Clinica

---
+++ src/main/java/co/edu/uniquindio/clinica/modelo/entidades/Medico.java
@@ -0,0 +1,6 @@
+package co.edu.uniquindio.clinica.modelo.entidades;
+
+import java.io.Serializable;
+
+public class Medico implements Serializable {
+}
---
+++ src/main/java/co/edu/uniquindio/clinica/modelo/entidades/Paciente.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.clinica.modelo.entidades;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Paciente implements Serializable {
+
+
+}
