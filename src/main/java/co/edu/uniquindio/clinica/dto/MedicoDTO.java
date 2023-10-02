2c94636b735fc253f561618825ba74603410657d
juanf28 <juan-felipe-00@hotmail.com>
2023-10-02 09:07:28 -0500

    solucion conflicto

--- src/main/java/co/edu/uniquindio/clinica/dto/MedicoDTO.java
+++ src/main/java/co/edu/uniquindio/clinica/dto/MedicoDTO.java
@@ -1,16 +1,37 @@
 package co.edu.uniquindio.clinica.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record MedicoDTO(
-    String nombre,
-    String cedula,
-    int codigoCiudad, String URL_foto,
-    int codigoEspecialidad,
-    String telefono,
-    String correo,
-    String password,
-    List< DiaTrabajoMedicoDTO > horarios
 
-){
+        @NotNull
+        String nombre,
+
+        @NotNull
+        String cedula,
+
+        @NotNull @Min(0) @Max(3)
+        int codigoCiudad,
+
+        @NotNull
+        String URL_foto,
+
+        @NotNull
+        int codigoEspecialidad,
+
+        @NotNull
+        String telefono,
+
+        @NotNull
+        String correo,
+
+        @NotNull
+        String password,
+        List<DiaTrabajoMedicoDTO> horarios
+
+) {
 }
